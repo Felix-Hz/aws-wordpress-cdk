0834bda41d0818d62b751c52622c6151463339fd
Félix Hernández Vieyra <71148989+Felix-Hz@users.noreply.github.com>
2024-03-31 19:20:02 +1300

    add: config env vars

--- bin/infra.ts
+++ bin/infra.ts
@@ -3,14 +3,13 @@ import "source-map-support/register";
 import { config } from "../lib/config";
 import { WpInfraStack } from "../lib/infra-stack";
 
-const app = new cdk.App({
-  context: {
-    // Sydney as main region.
-    region: "ap-southeast-2",
-  },
-});
+const app = new cdk.App();
 
 new WpInfraStack(app, `wordpressApp-${config.stage}`, {
+  env: {
+    account: config.aws_account.account_no,
+    region: config.aws_account.region,
+  },
   description:
     "Deploys and provisions infrastructure for a WordPress application",
   tags: { Project: config.projectName, DeployedBy: config.deployedBy },
--- lib/config.ts
+++ lib/config.ts
@@ -5,6 +5,10 @@ dotenv.config();
 const stage = process.env.STAGE || "dev";
 
 export const config = {
+  aws_account: {
+    account_no: process.env.ACCOUNT || "",
+    region: process.env.REGION || "",
+  },
   projectName: `wordpressApp-${stage}`,
   stage,
   deployedBy: `${process.env.DEPLOYED_BY || process.env.USER}`,
