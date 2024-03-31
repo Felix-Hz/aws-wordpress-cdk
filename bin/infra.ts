ca573a9522a43e1a5fdebf0783e2d249404eb28c
Félix Hernández Vieyra <71148989+Felix-Hz@users.noreply.github.com>
2024-03-31 16:39:16 +1300

    add: infra cdk region

--- bin/infra.ts
+++ bin/infra.ts
@@ -3,7 +3,13 @@ import "source-map-support/register";
 import { config } from "../lib/config";
 import { WpInfraStack } from "../lib/infra-stack";
 
-const app = new cdk.App();
+const app = new cdk.App({
+  context: {
+    // Sydney as main region.
+    region: "ap-southeast-2",
+  },
+});
+
 new WpInfraStack(app, `wordpressApp-${config.stage}`, {
   description:
     "Deploys and provisions infrastructure for a WordPress application",
