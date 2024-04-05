6023fcec3289088b5b8e244574ab6e04c50588a8
Félix Hernández Vieyra <71148989+Felix-Hz@users.noreply.github.com>
2024-04-05 13:23:21 +1300

    wip: integration tests

--- test/infra.test.ts
+++ test/infra.test.ts
@@ -1,17 +1,44 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Infra from '../lib/infra-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { WpInfraStack } from "../lib/infra-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/infra-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Infra.InfraStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+describe("Integration test: CMS CDK Infrastructure", () => {
+  let template: Template;
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  /* ================================================= *
+   *                  WORK IN PROGRESS                 *
+   * ================================================= */
+
+  beforeAll(() => {
+    const app = new cdk.App();
+    const stack = new WpInfraStack(app, "testTemplateStack");
+    template = Template.fromStack(stack);
+  });
+
+  test("VPC exists", () => {
+    // @BUG: Can't grab the resources, even if it evaluates the type.
+    expect(template.resourceCountIs("AWS::EC2::VPC", 1)).toBeTruthy();
+  });
+
+  // test("Security Groups exist", () => {
+  //   expect(template.resourceCountIs("AWS::EC2::SecurityGroup", 3)).toBeTruthy();
+  // });
+
+  // test("Bastion Host + Server exist", () => {
+  //   expect(template.resourceCountIs("AWS::EC2::Instance", 2)).toBeTruthy();
+  // });
+
+  // test("Aurora exists", () => {
+  //   expect(template.resourceCountIs("AWS::RDS::DBCluster", 1)).toBeTruthy();
+  // });
+
+  // test("ELB exists", () => {
+  //   expect(
+  //     template.resourceCountIs("AWS::ElasticLoadBalancingV2::LoadBalancer", 1)
+  //   ).toBeTruthy();
+  // });
+
+  // test("S3 exists", () => {
+  //   expect(template.resourceCountIs("AWS::S3::Bucket", 1));
+  // });
 });
